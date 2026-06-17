da3ecc1ae475a6be8484102d5585cfa1aa739e06
romay-angelo-1996 <romayangelodavid1996@gmail.com>
2026-06-17 16:14:39 +0800

    feat: set the initial project

---
+++ findKthLargest.js
@@ -0,0 +1,5 @@
+const findKthLargest = (nums, k) => {
+    // Your implementation here
+};
+console.log(findKthLargest([3, 2, 1, 5, 6, 4], 2)); // Output: 5
+console.log(findKthLargest([3, 2, 3, 1, 2, 4, 5, 5, 6], 4)); // Output
