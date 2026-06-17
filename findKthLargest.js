783a20553e4e0be45961ca36ca61784ceba91cd3
Romay <romayangelodavid1996@gmail.com>
2026-06-17 17:44:23 +0800

    chore: import function to test

--- findKthLargest.js
+++ findKthLargest.js
@@ -61,6 +61,6 @@ function partition(arr, start, end) {
   swap(arr, split, end);
   return split;
 }
-
+module.exports = { findKthLargest };
 console.log(findKthLargest([3, 2, 1, 5, 6, 4], 2)); // Output: 5
 console.log(findKthLargest([3, 2, 3, 1, 2, 4, 5, 5, 6], 4)); // Output
